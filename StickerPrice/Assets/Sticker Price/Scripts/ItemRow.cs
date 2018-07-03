4437206138c9d403c4f2b8ed5b24b38d9d408e2f
Garrett Moody <garrett.moody@snc.edu>
2018-07-02 23:39:36 -0500

    More improvements to Item Row.

--- StickerPrice/Assets/Sticker Price/Scripts/ItemRow.cs
+++ StickerPrice/Assets/Sticker Price/Scripts/ItemRow.cs
@@ -13,22 +13,42 @@ public class ItemRow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IP
 	private bool pointerDown = false;
 	private bool lerpToButtons = false;
 	private bool lerpToReset = false;
+	private bool lerpToDelete = false;
+	private bool lerpToPosition = false;
 	private float buttonOffset = 400;
+	private float deleteOffset = 800;
+	private float fullDeleteOffset = 1200;
 	private ItemList parentList; 
+	private float dragX;
 
 	void Update() {
 		if (!pointerDown) {
 			if (lerpToButtons) {
 				float newX = Mathf.Lerp (itemRectTransform.localPosition.x, -buttonOffset, Time.deltaTime * 5f);
 				itemRectTransform.localPosition = new Vector2 (newX, itemRectTransform.localPosition.y);
-				if (Mathf.RoundToInt(itemRectTransform.localPosition.x) == -buttonOffset) {
+				if (Mathf.RoundToInt (itemRectTransform.localPosition.x) == -buttonOffset) {
 					lerpToButtons = false;
 				}
 			} else if (lerpToReset) {
 				float newX = Mathf.Lerp (itemRectTransform.localPosition.x, 0f, Time.deltaTime * 5f);
 				itemRectTransform.localPosition = new Vector2 (newX, itemRectTransform.localPosition.y);
-				if (Mathf.RoundToInt(itemRectTransform.localPosition.x) == 0) {
+				if (Mathf.RoundToInt (itemRectTransform.localPosition.x) == 0) {
 					lerpToReset = false;
+				}
+			} else if (lerpToDelete) {
+				float newX = Mathf.Lerp (itemRectTransform.localPosition.x, -fullDeleteOffset, Time.deltaTime * 8);
+				itemRectTransform.localPosition = new Vector2 (newX, itemRectTransform.localPosition.y);
+				if (Mathf.RoundToInt (itemRectTransform.localPosition.x) == -fullDeleteOffset) {
+					lerpToDelete = false;
+				}
+			} else if (lerpToPosition) {
+				float newX = Mathf.Lerp (itemRectTransform.localPosition.x, dragX, Time.deltaTime * 8);
+				if (newX > 0) {
+					newX = 0;
+				}
+				itemRectTransform.localPosition = new Vector2 (newX, itemRectTransform.localPosition.y);
+				if (Mathf.RoundToInt (itemRectTransform.localPosition.x) == dragX) {
+					lerpToPosition = false;
 				}
 			}
 		}
@@ -40,33 +60,52 @@ public class ItemRow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IP
 		itemRectTransform = (RectTransform) this.transform;
 		lerpToReset = false;
 		lerpToButtons = false;
+		lerpToDelete = false;
+		lerpToPosition = false;
 		parentList = GetComponentInParent<ItemList> ();
 	}
 
 	public void OnPointerDown(PointerEventData data) {
 		lerpToReset = false;
 		lerpToButtons = false;
+		lerpToDelete = false;
+		lerpToPosition = false;
 		RectTransformUtility.ScreenPointToLocalPointInRectangle (itemRectTransform, data.position, data.pressEventCamera, out positionOffset);
 		parentList.resetOtherRows (this);
 	}
 
 	public void OnPointerUp(PointerEventData data) {
-		if (Mathf.RoundToInt(itemRectTransform.localPosition.x) < -buttonOffset) {
+		int localX = Mathf.RoundToInt (itemRectTransform.localPosition.x);
+		if ((localX < -buttonOffset && localX >= -deleteOffset) || (lerpToPosition && localX <= -deleteOffset)) {
+			lerpToPosition = false;
 			lerpToButtons = true;
-		} else if (Mathf.RoundToInt(itemRectTransform.localPosition.x) >= -buttonOffset) {
+		} else if (localX >= -buttonOffset) {
+			lerpToButtons = false;
+			lerpToPosition = false;
 			lerpToReset = true;
-		}
+		} else if (localX < -deleteOffset && !lerpToPosition) {
+			DeleteButtonOnClickListener ();
+		} 
 	}
 
 	public void OnDrag(PointerEventData data) {
 		Vector2 localPointerPosition;
 		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (canvasRectTransform, data.position, data.pressEventCamera, out localPointerPosition)) {
 			float newX = localPointerPosition.x + (itemRectTransform.rect.width / 2) - positionOffset.x;
-			if (newX > 0) {
+			dragX = newX;
+			//Debug.Log ("newX: " + newX + " localX: " + itemRectTransform.localPosition.x + " Delete: " + lerpToDelete + " Position: " + lerpToPosition);
+			if (newX > 0) { //trying to swipe right
 				newX = 0;
 			}
-
-			itemRectTransform.localPosition = new Vector2 (newX, this.transform.localPosition.y);
+			if (newX < -deleteOffset) { //activate delete
+				lerpToDelete = true;
+				lerpToPosition = false;
+			} else if (newX >= -deleteOffset && Mathf.RoundToInt(itemRectTransform.localPosition.x) < -deleteOffset) {
+				lerpToDelete = false; 
+				lerpToPosition = true;
+			} else {
+				itemRectTransform.localPosition = new Vector2 (newX, this.transform.localPosition.y);
+			}
 		}
 	}
 
