d201e010f69ac560962e7df1c5baf920756628c9
Garrett Moody <garrett.moody@snc.edu>
2018-10-13 13:44:40 -0500

    Changed QR Preview
    Made changes to the QR preview panel to have scalable components which overlap.

--- StickerPrice/Assets/Sticker Price/Scripts/QRPreview.cs
+++ StickerPrice/Assets/Sticker Price/Scripts/QRPreview.cs
@@ -8,7 +8,8 @@ public class QRPreview : MonoBehaviour {
     //Public Variables
     public float SNAPPING_THRESHOLD = 50f;
     public float LERP_SPEED = 5f;
-
+    public float SCALE_MAX_DISTANCE = 200f; //the max distance in which the component will no longer scale
+    public float SCALE_MIN_SCALE = .5f; //the smallest the component will scale to
     //Private Variables
     public ScrollRect scrollRect;
     private GameObject[] valueComponent;
@@ -16,7 +17,7 @@ public class QRPreview : MonoBehaviour {
     private Vector2 scrollOffset;
     public GameObject viewport;
     public GameObject content;
-
+    private List<float> distancesFromCenter = new List<float>();
 
     private bool isScrolling = false;
     private bool isMoving = false;
@@ -36,6 +37,9 @@ public class QRPreview : MonoBehaviour {
         }
        
         scrollOffset = valueComponentCenters[0];
+        LoadDistancesFromCenter();
+        ScaleComponents();
+        SortComponentLayer();
     }
 
     // Update is called once per frame
@@ -43,24 +47,9 @@ public class QRPreview : MonoBehaviour {
     {
         if (isMoving)
         { //if its not moving there is nothing to update. 
+            LoadDistancesFromCenter();
             if (!isScrolling)
             { //We are moving, but too slow to scroll. Time to lerp to position.
-                List<float> distancesFromCenter = new List<float>();
-
-                //do math for horizontal movement
-                if (horizontal)
-                {
-                    foreach (Vector2 position in valueComponentCenters)
-                    {
-                        distancesFromCenter.Add(Mathf.Abs(-content.transform.localPosition.x + scrollOffset.x - position.x));
-                    }
-                } else { //math for verticle movement
-                    foreach (Vector2 position in valueComponentCenters)
-                    {
-                        distancesFromCenter.Add(Mathf.Abs(-content.transform.localPosition.y + scrollOffset.y - position.y));
-                    }
-                }
-
                 //get smallest distance from center
                 float[] distanceArray = distancesFromCenter.ToArray();
                 float minDistance = Mathf.Min(distanceArray);
@@ -68,6 +57,88 @@ public class QRPreview : MonoBehaviour {
                 int minIndex = distancesFromCenter.IndexOf(minDistance);
                 Vector2 newPosition = new Vector2(-valueComponentCenters[minIndex].x + scrollOffset.x, valueComponentCenters[minIndex].y - scrollOffset.y);
                 LerpToFinish(newPosition);
+            }
+
+            ScaleComponents();
+            SortComponentLayer();
+        }
+    }
+
+    void LoadDistancesFromCenter() {
+        distancesFromCenter.Clear();
+        //do math for horizontal movement
+        if (horizontal)
+        {
+            foreach (Vector2 position in valueComponentCenters)
+            {
+                distancesFromCenter.Add(Mathf.Abs(-content.transform.localPosition.x + scrollOffset.x - position.x));
+            }
+        }
+        else
+        { //math for verticle movement
+            foreach (Vector2 position in valueComponentCenters)
+            {
+                distancesFromCenter.Add(Mathf.Abs(-content.transform.localPosition.y + scrollOffset.y - position.y));
+            }
+        }
+
+        return;
+    }
+
+    void ScaleComponents() {
+        //For each component, change the scale accordingly
+        for (int i = 0; i < valueComponent.Length; i++)
+        {
+
+            //if the component is within scalable threshold to be scaled
+            if (distancesFromCenter[i] < SCALE_MAX_DISTANCE)
+            {
+                //Figure out the scale of the component by how far away the component is from the center
+                float scale = (SCALE_MAX_DISTANCE - distancesFromCenter[i]) / SCALE_MAX_DISTANCE * SCALE_MIN_SCALE + SCALE_MIN_SCALE;
+                //Set the scale of the component. 
+                valueComponent[i].gameObject.GetComponent<RectTransform>().localScale = new Vector3(scale, scale, 1);
+            }
+            else //component too far away, set the component to the min scale
+            {
+                valueComponent[i].gameObject.GetComponent<RectTransform>().localScale = new Vector3(SCALE_MIN_SCALE, SCALE_MIN_SCALE, 1f);
+            }
+        }
+    }
+
+    void SortComponentLayer() {
+        //Find the object closest to the center
+        float[] distanceArray = distancesFromCenter.ToArray();
+        float minDistance = Mathf.Min(distanceArray);
+
+        int minIndex = distancesFromCenter.IndexOf(minDistance);
+
+        //Component closest to center should be on top
+        valueComponent[minIndex].gameObject.GetComponent<Canvas>().sortingOrder = valueComponent.Length;
+
+        //This process starts from the center and moves outward. The component in the center gets the highest 
+        //sorting order. The components on both sides get the sorting order - 1. Components outside of that 
+        //get sorting order - 2 and so on until there are no more components on either side. 
+        bool processing = true; //keep track of while loop
+        int sortingOrder = valueComponent.Length - 1; 
+        int layer = 1; //keeps track of what layer we are on. 
+        while (processing)
+        {
+            if (minIndex - layer >= 0)
+            {
+                valueComponent[minIndex - layer].gameObject.GetComponent<Canvas>().sortingOrder = sortingOrder;
+            }
+            if (minIndex + layer < valueComponent.Length)
+            {
+                valueComponent[minIndex + layer].gameObject.GetComponent<Canvas>().sortingOrder = sortingOrder;
+            }
+            if (minIndex - layer < 0 && minIndex + layer >= valueComponent.Length)
+            {
+                processing = false;
+            }
+            else
+            {
+                sortingOrder--;
+                layer++;
             }
         }
     }
