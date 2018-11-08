48d68e43ae9264eb7125e38e17f62a04b5b8a9d3
Garrett Moody <garrett.moody@snc.edu>
2018-11-07 21:45:47 -0600

    Reskinned the Home Page

---
+++ StickerPrice/Assets/Sticker Price/Scripts/Toolbar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toolbar : MonoBehaviour {
+
+    public Button createButton;
+    public Button printButton;
+    public Button scanButton;
+    public Button trackButton;
+    public Button moreButton;
+    public Canvas canvas;
+    public GameObject stickerFormatMenu;
+    public GameObject scanPanel;
+    public GameObject readyForPrint;
+    public GameObject moreMenu;
+
+    public void CreateButtonOnClickListener()
+    {
+        DeactivateAllPages();
+        stickerFormatMenu.gameObject.SetActive(true);
+    }
+
+    public void PrintButtonOnClickListener()
+    {
+       // DeactivateAllPages();
+    }
+
+    public void ScanButtonOnClickListener()
+    {
+        DeactivateAllPages();
+        scanPanel.gameObject.SetActive(true);
+    }
+
+    public void TrackButtonOnClickListener()
+    {
+        DeactivateAllPages();
+    }
+
+    public void MoreButtonOnClickListener()
+    {
+        DeactivateAllPages();
+        moreMenu.gameObject.SetActive(true);
+    }
+
+    private void DeactivateAllPages() {
+        foreach(Transform page in canvas.transform) {
+            if(page.gameObject != this.gameObject)
+            {
+                page.gameObject.SetActive(false);
+            }
+        }
+    }
+}
