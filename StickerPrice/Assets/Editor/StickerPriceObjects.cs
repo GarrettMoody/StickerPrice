4a876c4f9a271ae6db439cdf6b590e853ff64044
Garrett Moody <garrett.moody@snc.edu>
2018-11-10 13:00:32 -0600

    Created Content Menu
    Added content menu to home page and made unity editor changes.

---
+++ StickerPrice/Assets/Editor/StickerPriceObjects.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class StickerPriceObjects : MonoBehaviour {
+
+    [MenuItem("GameObject/UI/StickerPrice/Content Scroll")]
+    private static void CreateContentScroll() {
+        ContentScroll contentScrollPrefab = (ContentScroll)AssetDatabase.LoadAssetAtPath("Assets/Sticker Price/Prefabs/ContentScroll.prefab", typeof(ContentScroll));
+        ContentScroll newScroll = Instantiate<ContentScroll>(contentScrollPrefab);
+        newScroll.transform.SetParent(Selection.activeGameObject.transform);
+    }
+}
