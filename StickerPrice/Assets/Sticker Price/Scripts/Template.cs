074c786bd5ad5ee07016d26088bee839b12020d3
Neema Al Ameen <neema.alameen@gmail.com>
2019-02-16 13:05:57 -0600

    Template class added

---
+++ StickerPrice/Assets/Sticker Price/Scripts/Template.cs
@@ -0,0 +1,36 @@
+﻿-﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Template
+{
+    public string templateId;
+    public string size;
+    public string qrCode;
+    public string numberPerSheet;   
+
+    public Template ()
+    {
+        this.templateId = "";
+        this.size = "";
+        this.qrCode = "";
+        this.numberPerSheet = "";
+    }
+
+    public Template(string templateId)
+    {
+        this.templateId = templateId;
+        this.size = "";
+        this.qrCode = "";
+        this.numberPerSheet = "";
+    }
+    
+    public Template(string templateId, string size, string qrCode, string numberPerSheet)
+    {
+        this.templateId = templateId;
+        this.size = size;
+        this.qrCode = qrCode;
+        this.numberPerSheet = numberPerSheet;
+    }
+}
