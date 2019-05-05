6213e9c8ec2d2048208a5b3b7007a76dd59561f4
Neema Al Ameen <neema.alameen@gmail.com>
2019-05-05 13:32:43 -0500

    Removed from ignored list

---
+++ StickerPrice/Assets/Sticker Price/Scripts/AddItemPopup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Globalization;
+
+public class AddItemPopup: MonoBehaviour
+{
+    public GameObject popupPanel;
+    public ScanPanel scanPanel;
+    public InputField itemDescription;
+    public InputField productOwner;
+    public InputField price;
+
+    public void SaveButtonOnClickListener()
+    {
+        Sticker sticker = new Sticker();
+        float itemPrice = float.Parse(price.text, NumberStyles.Currency);
+
+        sticker.itemDescription = itemDescription.text;
+        sticker.owner = productOwner.text;
+        sticker.price = itemPrice.ToString();
+
+        scanPanel.AddItem(sticker);
+
+        this.gameObject.SetActive(false);
+        popupPanel.gameObject.SetActive(false);
+    }
+
+    public void CancelButtonOnClickListener()
+    {
+        this.gameObject.SetActive(false);
+        popupPanel.gameObject.SetActive(false);
+    }
+}
